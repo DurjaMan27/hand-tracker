7bd18adbb8376b468cebe9f093838bbbf8bbcc34
Varun Rao <varunvellorerao@gmail.com>
2025-01-27 22:22:45 -0500

    intro goals

---
+++ goals.py
@@ -0,0 +1,17 @@
+# Using:
+  # OpenCV
+  # Darknet (YOLO)
+  # Matplotlib 3D
+  # PyMesh
+
+# Requirements:
+  # live camera
+  # hand
+  # some 3D object already online
+
+# Overall Goals:
+  # using OpenCV's handtracking software (along with other relevant libraries)
+  # use hand position and orientation to move a 3D object
+  # this 3D object can be 3D graph data, or some object that requires 3D visualization
+  # object will change position and orientation according to hand positions/orientations
+  # this includes turning your hand around, closing your fingers, showing fingers, etc.
