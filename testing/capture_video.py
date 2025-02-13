d7837f49cd52a56da31f56c99f8b6103ed79c4ae
Varun Rao <varunvellorerao@gmail.com>
2025-02-12 23:04:26 -0500

    getting started

---
+++ testing/capture_video.py
@@ -0,0 +1,29 @@
+import cv2
+
+# open the default camera
+cam = cv2.VideoCapture(0)
+
+# Get the default frame width and height
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# define the code and create the VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('GitHubCode/hand-tracker/output.mp4', fourcc, 20.0, (frame_width, frame_height))
+
+while True:
+  ret, frame, = cam.read()
+
+  # Write the frame to the output file
+  out.write(frame)
+
+  # Display the captured frame
+  cv2.imshow('Camera', frame)
+
+  # press 'q' to exit the loop
+  if cv2.waitKey(1) == ord('q'):
+    break
+
+cam.release()
+out.release()
+cv2.destroyAllWindows()
