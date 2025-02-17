82099244bc35b83f3b6fbbd30e42d52be329cabc
Varun Rao <varunvellorerao@gmail.com>
2025-02-16 19:29:15 -0500

    can't figure out video writing

--- testing/capture_video.py
+++ testing/capture_video.py
@@ -8,8 +8,8 @@ frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # define the code and create the VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('GitHubCode/hand-tracker/output.mp4', fourcc, 20.0, (frame_width, frame_height))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+out = cv2.VideoWriter('/Users/varunrao/GitHubCode/hand-tracker/testing/output.mp4', fourcc, 20.0, (int(cam.get(3)),int(cam.get(4))))
 
 while True:
   ret, frame, = cam.read()
