a4f28fc1b9c7c57eaea07f6b0bad899c22805245
Varun Rao <varunvellorerao@gmail.com>
2025-07-22 20:37:00 -0400

    reorganizing

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5500;
+
+app.use(express.static(path.join(__dirname, 'website')));
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'website', 'index.html'));
+});
+
+app.get('/:page', (req, res) => {
+  const filePath = path.join(__dirname, 'website', `${req.params.page}.html`);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.sendFile(path.join(__dirname, 'website', 'index.html'))
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
